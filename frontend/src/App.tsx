4a7fc91ac28afa06e5b6409b1af1d0f8216b4fca
Valter Junior <valterpcjr@gmail.com>
2022-09-09 15:41:06 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,19 @@
+import NotifictionButton from "./components/NotificationButton"
+
 function App() {
  return (
-  <h1>Olá Mundo!</h1>
+  <>
+  <h1>Olá Mundo! Botão A</h1>
+  <h3>Bem vindo ao mundo React</h3>
+  <br/>
+  <NotifictionButton/>
+  <br/>
+  <h1>Olá Mundo! Botão B</h1>
+  <NotifictionButton/>
+  <br/>
+  <h1>Olá Mundo! Botão C</h1>
+  <NotifictionButton/>
+  </>
  )
 }
 
